901f90d4a2a736f4f907ee5d0e0f36e8ace9bf29
shivani chhatre <shivanichhatre@gmail.com>
2022-06-16 16:08:26 +0530

    home page and recovery password page created up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,15 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'recover-password',
+    loadChildren: () => import('./recover-password/recover-password.module').then( m => m.RecoverPasswordPageModule)
   }
+
 ];
 @NgModule({
   imports: [
